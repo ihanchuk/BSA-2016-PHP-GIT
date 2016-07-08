c1ae9f7f1c4bb8f31693e0012a62ff4e2c2af888
ihanchuk <mfc2005@ukr.net>
2016-07-08 13:45:53 +0300

    0001: Modified gitignore file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
+/* Require section*/
 var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'),
     concatCss = require('gulp-concat-css'),
     rename = require("gulp-rename"),
     autoprefixer = require('gulp-autoprefixer');
- 
+
+/* Gulp tasks*/
 gulp.task('default', function () {
   return gulp.src('lib/css/*.css')
     .pipe(concatCss("styles/index.css"))
