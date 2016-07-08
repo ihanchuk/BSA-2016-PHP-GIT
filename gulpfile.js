3c8f3da2455b22b3e814235f98ce9b07e89394f2
ihanchuk <mfc2005@ukr.net>
2016-07-08 13:41:39 +0300

    0000: Initial project structure

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+    cleanCSS = require('gulp-clean-css'),
+    concatCss = require('gulp-concat-css'),
+    rename = require("gulp-rename"),
+    autoprefixer = require('gulp-autoprefixer');
+ 
+gulp.task('default', function () {
+  return gulp.src('lib/css/*.css')
+    .pipe(concatCss("styles/index.css"))
+    .pipe(autoprefixer({
+			browsers: ['last 6 versions'],
+			cascade: false
+		}))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+     .pipe(rename("styles/index-min.css"))
+    .pipe(gulp.dest('.'));
+});
